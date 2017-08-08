0374f750d0035db7a845a6ee83a5132a5033ae75
Shodhana <30806758+Shodhana@users.noreply.github.com>
2017-08-08 22:42:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var ele=document.getElementById('main');
 ele.innerHTML="Boooo Yaaaa!";
+
+
+//image
+var image=document.getElementById('madi');
+image.onClick=function () {
+    image.style.marginLeft="100px";
+}
