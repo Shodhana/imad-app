a168e4fe0df0f2bb66ebaf6fcf71904a486e00ad
Shodhana <30806758+Shodhana@users.noreply.github.com>
2017-08-08 22:57:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ ele.innerHTML="Boooo Yaaaa!";
 
 //image
 var img=document.getElementById('madi');
+var marginLeft=0;
 function moveRight() {
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
