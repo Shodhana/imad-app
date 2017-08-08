3ed0bfef53e6a079bdb153d226473743c7ee5946
Shodhana <30806758+Shodhana@users.noreply.github.com>
2017-08-08 23:29:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,8 @@ var btn=document.getElementById('counter');
 var c=0;
 btn.onclick = function () {
     
+    //make req to counter
+    var req=new XMLHttpRequest();
     
     //pass counter variable to span value
     c=c+1;
