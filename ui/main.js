18fdd4c3f1a38261a69ecf18ff224d94d811a51e
Shodhana <30806758+Shodhana@users.noreply.github.com>
2017-08-08 23:20:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,5 @@
 console.log('Loaded!');
-var ele=document.getElementById('main');
-ele.innerHTML="Boooo Yaaaa!";
 
-
-//image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight() {
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-
-img.onclick=function () {
-    var intrvl=setInterval(moveRight,50);
-     
-}
 
 var button=document.getElementById('counter');
 button.onclick=function () {
