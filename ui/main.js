96c2f473849663dc86c864db3f71b871161fd1ff
Shodhana <30806758+Shodhana@users.noreply.github.com>
2017-08-08 23:25:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,13 @@ console.log('Loaded!');
 
 
 var btn=document.getElementById('counter');
+var c=0;
 btn.onclick = function () {
     
-    var c=0;
+    
     //pass counter variable to span value
-    var span=document.getElementById('count');
     c=c+1;
+    var span=document.getElementById('count');
+   
     span.innerHTML=c.toString();
-}
+};
